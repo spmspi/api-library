aa1381ac974935d2c72e12d80abdfacd298175c3
SPMSPI <kislicin.artur@gmail.com>
2026-08-17 06:53:26 +0300

    Adedd author models

--- books/models.py
+++ books/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
 class Book(models.Model):
     class CoverChoices(models.TextChoices):
         HARD = "Hard"
         SOFT = "Soft"
     title = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
     cover = models.CharField(
         max_length=5,
         choices=CoverChoices.choices,)
@@ -14,4 +23,3 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
-
