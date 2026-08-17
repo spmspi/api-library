007855a7f23087eacfda357d7ff1cf6cab9a250e
SPMSPI <kislicin.artur@gmail.com>
2026-08-17 06:36:03 +0300

    Create 'Books sevice' and add nodel

---
+++ Books/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Book(models.Model):
+    class CoverChoices(models.TextChoices):
+        HARD = "Hard"
+        SOFT = "Soft"
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    cover = models.CharField(
+        max_length=5,
+        choices=CoverChoices.choices,)
+    Inventory = models.PositiveIntegerField()
+    daily_free = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.title
