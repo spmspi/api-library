7b16756f014a570d83ea68ba737c31138df22934
SPMSPI <kislicin.artur@gmail.com>
2026-08-18 08:19:14 +0300

    create model borrowing

--- books_service/models.py
+++ books_service/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from books.models import Book
+from user.models import User
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField()
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField()
+    book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
