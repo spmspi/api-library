e07e8298484d5e62063ee9cd2a40181a1840181c
SPMSPI <kislicin.artur@gmail.com>
2026-08-19 06:54:21 +0300

    adedd validation inventory book not in 0 & create BorrowingCreateSerializer

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from books.models import Book
 from books.serializers import BookSerializer
 from borrowing.models import Borrowing
 from user.serializers import UserSerializer
@@ -7,14 +8,30 @@ from user.serializers import UserSerializer
 
 class BorrowingSerializer(serializers.ModelSerializer):
     book = BookSerializer(read_only=True)
-    user = UserSerializer(read_only=True)
 
     class Meta:
         model = Borrowing
-        fields = ("borrow_date", "book", "user")
+        fields = ("borrow_date", "expected_return_date", "book")
 
 class BorrowingDetailSerializer(BorrowingSerializer):
+    user = UserSerializer(read_only=True)
     class Meta:
         model = Borrowing
         fields = ("borrow_date", "expected_return_date", "book", "user")
+
+class BorrowingCreateSerializer(BorrowingSerializer):
+    book = serializers.PrimaryKeyRelatedField(
+        queryset=Book.objects.all()
+    )
+    class Meta:
+        model = Borrowing
+        fields = (
+            "book",
+            "expected_return_date",
+        )
+
+    def validate_book(self, value):
+        if value.inventory <= 0:
+            raise serializers.ValidationError("The book is out of stock.")
+        return value
 
--- borrowing/views.py
+++ borrowing/views.py
@@ -1,8 +1,10 @@
+from django.db import transaction
 from django.shortcuts import render
 from rest_framework import viewsets
 
+from books.models import Book
 from borrowing.models import Borrowing
-from borrowing.serializers import BorrowingSerializer
+from borrowing.serializers import BorrowingSerializer, BorrowingDetailSerializer, BorrowingCreateSerializer
 
 
 class BorrowingViewSet(viewsets.ModelViewSet):
@@ -14,3 +16,19 @@ class BorrowingViewSet(viewsets.ModelViewSet):
         if not self.request.user.is_staff:
             return queryset.filter(user=self.request.user)
         return queryset
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BorrowingSerializer
+        if self.action == "create":
+            return BorrowingCreateSerializer
+        elif self.action == "retrieve":
+            return BorrowingDetailSerializer
+        return BorrowingSerializer
+
+    def perform_create(self, serializer):
+        with transaction.atomic():
+            book = serializer.validated_data["book"]
+            book.inventory -= 1
+            book.save()
+            serializer.save(user=self.request.user)
