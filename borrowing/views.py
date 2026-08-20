71c7291d54760c60b248e0e59a5e814c3fd1f6d1
SPMSPI <kislicin.artur@gmail.com>
2026-08-20 07:33:16 +0300

    adedd filter in borrowing for user and actual return date

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,13 +1,11 @@
 from datetime import date
 
 from django.db import transaction
-from django.shortcuts import render
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from books.models import Book
 from borrowing.models import Borrowing
 from borrowing.serializers import BorrowingSerializer, BorrowingDetailSerializer, BorrowingCreateSerializer
 
@@ -16,11 +14,35 @@ class BorrowingViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.select_related("book", "user").all()
     serializer_class = BorrowingSerializer
 
+    @staticmethod
+    def _params_to_ints(qs):
+        """Converts a list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in qs.split(",")]
+
     def get_queryset(self):
+        current_user = self.request.user
         queryset = self.queryset
-        if not self.request.user.is_staff:
-            return queryset.filter(user=self.request.user)
+
+        if not current_user.is_staff:
+            return queryset.filter(user=current_user)
+
+        user_param = self.request.query_params.get("user")
+
+        if user_param:
+            user_ids = self._params_to_ints(user_param)
+            if user_ids:
+                queryset = queryset.filter(user_id__in=user_ids)
+
+        is_active = self.request.query_params.get("is_active")
+
+        if is_active is not None:
+            if is_active.lower() in "true":
+                queryset = queryset.filter(actual_return_date__isnull=True)
+            elif is_active.lower() in "false":
+                queryset = queryset.filter(actual_return_date__isnull=False)
+
         return queryset
+
 
     def get_serializer_class(self):
         if self.action == "list":
