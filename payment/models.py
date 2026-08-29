fd974699c5ff60271639d2f04ee39aa047233e34
SPMSPI <kislicin.artur@gmail.com>
2026-08-29 07:22:26 +0300

    read in default status -> pending and view get qeruset & add perform create

--- payment/models.py
+++ payment/models.py
@@ -11,7 +11,7 @@ class Payment(models.Model):
         FINE = "Fine"
 
     status = models.CharField(
-        choices=StatusEnum.choices, max_length=10, default=TypeEnum.PAYMENT
+        choices=StatusEnum.choices, max_length=10, default=TypeEnum.PENDING
     )
     type = models.CharField(choices=TypeEnum.choices, max_length=10)
     borrowing = models.ForeignKey(
--- payment/views.py
+++ payment/views.py
@@ -1,4 +1,8 @@
 from rest_framework import viewsets
+from django.db import transaction
+
+import payment
+from borrowing.task import send_notification_task
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -13,13 +17,24 @@ class PaymentViewSet(viewsets.ModelViewSet):
         current_user = self.request.user
         queryset = self.queryset
 
-        if not current_user.is_staff:
-            return queryset.filter(user=current_user)
+        if current_user.is_staff:
+            return queryset
+        return queryset.filter(borrowing__user=current_user)
 
     def get_serializer_class(self):
         if self.action == "detail":
             return PaymentDetailSerializer
         return PaymentSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+        message = (
+            f"<b>New payments!</b>\n"
+            f"Book: {payment.borrowing.book.title}\n"
+            f"User: {self.request.user}\n"
+            f"Expected return date: {payment.money}\n"
+        )
+        transaction.on_commit(lambda: send_notification_task.delay(message))
 
 
 class PaymentSuccessView(APIView):
